0953a406e74a44416bd448273e336946a586e996
Serge01700 <waxqo69@gmail.com>
2025-04-29 13:59:20 +0200

    my frist components

---
+++ dashboard-api/src/components/BulletPoint.vue
@@ -0,0 +1,26 @@
+<template>
+    <figure  class="bullet-point flex items-center w-20 h-10 justify-center rounded-full ">
+        <img :style="{ width: size, height:size}"  class="object-contain w-full h-full" :src="image" >
+    </figure>
+  
+</template>
+
+<script setup>
+defineProps({
+    image: String,
+
+    alt:{
+        type: String,
+        default: 'Bullet icon'
+    },
+    size:{
+        type: String,
+        default: '40px'
+    }
+})
+
+</script>
+
+<style scoped>
+
+</style>
