93b4da816562b000999441244eb1a8f70b19ee26
Serge01700 <waxqo69@gmail.com>
2025-04-29 16:09:37 +0200

    in progress

---
+++ dashboard-api/src/components/Sidebar.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Bonjour</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ dashboard-api/src/router/index.js
@@ -0,0 +1,3 @@
+import { createRouter, createWebHoistory } from 'vue-router'
+import DashboardLayout from '@layout/DashboardLayout.vue'
+import Home from '/page/HomePage.vue'
