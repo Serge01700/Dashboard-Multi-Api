5e9b37470b18523a95090377c51cb623d4ce3894
Serge01700 <waxqo69@gmail.com>
2025-05-06 10:53:38 +0200

    auth

---
+++ dashboard-api/src/page/Register.vue
@@ -0,0 +1,24 @@
+<template>
+  <animated-background-layout>
+    <div class="register-container">
+        <div class="back-link">
+            <RouterLink to="/" class="text-white">← Back to home</RouterLink>
+        </div>
+        <RegisterForm/>
+    </div>
+    
+
+
+  </animated-background-layout>
+</template>
+
+<script setup>
+import AnimatedBackgroundLayout from '@/layouts/AnimatedBackgroundLayout.vue';
+import RegisterForm from '@/components/RegisterForm.vue';
+
+
+</script>
+
+<style>
+
+</style>
