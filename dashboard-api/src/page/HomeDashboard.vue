73a32abbe0771e73664c9425a56326f789b7e7fa
Serge01700 <waxqo69@gmail.com>
2025-06-18 10:44:07 +0200

    to do

--- dashboard-api/src/page/HomeDashboard.vue
+++ dashboard-api/src/page/HomeDashboard.vue
@@ -1,10 +1,13 @@
 <template>
-  <WidgetWeatherHome :isDarkMode="isDarkMode"/>
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-6 p-6">
+    <WidgetWeatherHome :isDarkMode="isDarkMode"/>
+    <RecentTasks :isDarkMode="isDarkMode" />
+  </div>
 </template>
 
 <script setup>
-import WidgetWeatherHome  from '@/components/WidgetWeatherHome.vue';
-
+import WidgetWeatherHome from '@/components/WidgetWeatherHome.vue';
+import RecentTasks from '@/components/RecentTasks.vue';
 
 const props = defineProps({
   isDarkMode: {
