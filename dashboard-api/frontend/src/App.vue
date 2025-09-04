6966dc6d706d9543d4c9448b6beeae9563c734a9
Serge01700 <waxqo69@gmail.com>
2025-09-04 09:25:02 +0200

    persistance d'état vudraggable corriger,

--- dashboard-api/frontend/src/App.vue
+++ dashboard-api/frontend/src/App.vue
@@ -12,16 +12,22 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { useAuthStore } from './stores/auth'
 import WidgetWeather from './components/WidgetWeatherHome.vue'
 import Sidebar from './components/Sidebar.vue'
 import HomePage from './page/HomePage.vue'
 
+const authStore = useAuthStore()
 const isDarkMode = ref(true)
 
 const toggleTheme = () => {
     isDarkMode.value = !isDarkMode.value
 }
+
+onMounted(async () => {
+    await authStore.init()
+})
 </script>
 
 <style>
